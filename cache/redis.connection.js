08f09b71d858c9b1f5ce1ddfd4579ad1b3561cf1
Zahyr Seferina <zahyr.seferina@gmail.com>
2021-08-16 15:37:07 +0200

    Refactor: changed url connection string type

--- cache/redis.connection.js
+++ cache/redis.connection.js
@@ -1,14 +1,6 @@
 var cache = require('express-redis-cache');
+const redis = require("redis");
 
-const redisConnectionString = process.env.REDIS_URL.split(':');
-const redisHost = redisConnectionString[0];
-const redisPort = redisConnectionString[1];
-
-var client = cache({
-    host: redisHost,
-    port: redisPort,
-    auth_pass: process.env.REDIS_PASSWORD,
-    expire: 5000
-});
+var client = cache(redis.createClient(process.env.REDIS_URL));
 
 module.exports = client;
